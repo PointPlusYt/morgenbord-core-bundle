e5f24aa59ec8e63e4608d750b6f43feffa7abd9c
Djyp Forest Fortin <djyp@plus.yt>
2025-02-13 14:36:56 +0100

    Deleting JS

--- src/Resources/public/fonts.js
+++
@@ -1,5 +0,0 @@
-import "@fontsource/fira-sans/300.css";
-import "@fontsource/fira-sans/400.css";
-import "@fontsource/fira-sans/700.css";
-
-// import "fork-awesome/css/fork-awesome.min.css";
--- src/Resources/public/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// let tailwindcss = require('tailwindcss');
-
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {
-      config: './assets/bundles/core/tailwind.config.js',
-    },
-  },
-};
