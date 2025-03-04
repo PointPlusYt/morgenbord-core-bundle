5171e6b7d508715a5fc02577a576f401e5ff68d8
Djyp Forest Fortin <djyp@plus.yt>
2025-03-05 00:29:56 +0100

    🔥 Unused Directory declared in services.yaml

--- src/Controller/Api/V0/WidgetController.php
+++ src/Controller/Api/V0/WidgetController.php
@@ -2,10 +2,9 @@
 
 namespace Morgenbord\CoreBundle\Controller\Api\V0;
 
-use Morgenbord\CoreBundle\Entity\User;
-use Morgenbord\CoreBundle\Entity\UserWidget;
-use Morgenbord\CoreBundle\Widget\Registration;
 use Doctrine\ORM\EntityManagerInterface;
+use Morgenbord\CoreBundle\Entity\UserWidget;
+use Morgenbord\CoreBundle\Service\WidgetRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,27 +13,9 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/api/v0/widget', name: 'api_v0_widget')]
 class WidgetController extends AbstractController
 {
-    private $request;
-    private $widgets;
-
-    public function __construct(RequestStack $requestStack)
-    {
-        $this->request = $requestStack->getCurrentRequest();
-        $this->widgets = $requestStack->getCurrentRequest()->widgets;
-    }
-
-    // #[Route('', name: '_register', methods: ['POST'])]
-    // public function register(Registration $widgetRegistration): Response
-    // {        
-    //     // get widget details from form or json
-    //     $widgetDetails = json_decode($this->request->getContent(), true);
-    //     // TEMP TO GET actual user
-    //     // $this->getUser();
-    //     $user = $this->manager->getRepository(User::class)->findOneBy([]);
-    //     $userWidget = $widgetRegistration->addUserWidget($widgetDetails, $user);
-
-    //     return $this->json([$this->widgets, $userWidget], 200, [], ['groups' => ['read']]);
-    // }
+    public function __construct(
+        private WidgetRegistry $widgetRegistry
+    ) { }
 
     #[Route('/data/{id}', name: 'get_widget_data', methods: ['GET'])]
     public function getWidgetData(UserWidget $userWidget): Response
